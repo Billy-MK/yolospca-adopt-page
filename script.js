bc888628cf33a49915d2e4d33fcded80d688c74b
Billy Keating <wmontoothkeating@gmail.com>
2021-09-30 16:43:09 -0700

    Animal search back end

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var data = {
-    "apikey" : "API_KEY_HERE",
-    "objectType" : "animals",
-    "objectAction" : "define"
+    "apikey" : 'bos596JH',
+        "objectType" : "animals",
+        "objectAction" : "publicSearch",
+        "search" : {
+            "resultStart" : 0,
+            "resultLimit" : 8,
+            "resultSort" : "animalID",
+            "resultOrder" : "asc",
+            "calcFoundRows" : "Yes",
+            "filters" : {
+                },
+            "fields": ["animalID","animalAgeString", "animalGeneralAge", "animalBreed","animalDescriptionPlain","animalLocationCitystate","animalName","animalPrimaryBreed","animalSpecies","animalThumbnailUrl","animalUrl","animalPictures"]
+        }
 }
 
 fetch('https://api.rescuegroups.org/http/v2.json', {
@@ -9,4 +19,4 @@ fetch('https://api.rescuegroups.org/http/v2.json', {
     body: JSON.stringify(data)
 })
   .then(response => response.json())
-  .then(data => console.log(data.data));
+  .then(data => console.log(data));
