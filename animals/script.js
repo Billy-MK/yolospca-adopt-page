73efe640f5b9c9998960f33d1b172b52aa2f2c71
Billy Keating <wmontoothkeating@gmail.com>
2021-10-25 19:18:15 -0700

    Search function via rescuegroups api based on animalID Param complete, currently retrieving same data as search page but will add more fields as needed

---
+++ animals/script.js
@@ -0,0 +1,43 @@
+// Getting the ID of the animal to be searched for
+
+var animalIDParam = new URLSearchParams(window.location.search)
+var animalID = animalIDParam.get('id');
+
+// Searching for the animal and returning the relevant data
+
+var animalName;
+var animalSex;
+var animalBreed;
+var animalGeneralAge;
+var animalPictures;
+
+fetch('https://api.rescuegroups.org/http/v2.json', {
+    method: 'POST',
+    body: JSON.stringify({
+        "apikey" : '',
+            "objectType" : "animals",
+            "objectAction" : "publicSearch",
+            "search" : {
+                "resultStart" : 0,
+                "resultLimit" : 1,
+                "resultSort" : "animalID",
+                "resultOrder" : "asc",
+                "calcFoundRows" : "Yes",
+                "filters" : [{
+                    "fieldName" : "animalID",
+                    "operation" : "equals",
+                    "criteria" : animalID
+                }],
+                "fields": ["animalName", "animalSex", "animalBreed", "animalGeneralAge", "animalPictures"]
+            }
+    })
+})
+  .then(response => response.json())
+  .then(data => {
+      console.log(data.data)
+    animalName = data.data[animalID].animalName;
+    animalSex = data.data[animalID].animalSex;
+    animalBreed = data.data[animalID].animalBreed;
+    animalGeneralAge = data.data[animalID].animalGeneralAge;
+    animalPictures = data.data[animalID].animalPictures;
+  })
